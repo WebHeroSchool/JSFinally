b0dbceb60d4de62dfb0b54174a36c05e4ccf6899
Sveta Mitukhina <s.tu.tu.908@gmail.com>
2019-10-02 11:05:26 +0300

    fierst page

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 console.log('hello');
+const buttonSimple = document.getElementById('item__simple');
+const buttonMiddle = document.getElementById('item__medium');
+const buttonHard = document.getElementById('item__hard');
+const menuLevel = document.getElementById('containerFromLevel');
+const buttonLevel = document.querySelectorAll('.main_menu__item');
+
 //Стартовая страница. Добавляем элементу стили при наведении.
 
-//const addHoverOnElement = () => {
-//    const items = 
-//    
-//    
-//}
+
+  for (let i = 0; i < buttonLevel.length; i++) {
+    buttonLevel[i].addEventListener("click", function() {
+      let flow = document.getElementsByClassName("checked");
+      flow[0].className = flow[0].className.replace("checked", "");
+      this.className += " checked";
+    });
+  }
